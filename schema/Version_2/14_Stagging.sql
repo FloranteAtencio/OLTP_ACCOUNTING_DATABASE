dbf4f39e7dd8a26a9615a973caef12bf92a60e11
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-07-27 03:26:54 +0800

    CHECK FOR WHERE IS MISSING

--- schema/Version_2/14_Stagging.sql
+++ schema/Version_2/14_Stagging.sql
@@ -185,11 +185,11 @@ BEGIN
         '
         UPDATE Staging.import_workflows a
         SET
-            a.new_state = %L,
-            a.previous_state = %L,
-            a.notes = %L
+            new_state = %L,
+            previous_state = %L,
+            notes = %L
         FROM %s b 
-        AND a.session_id = b.session_id
+        WHERE a.session_id = b.session_id
         AND b.session_id = %L
         AND b.validation_status = %L;
         ',
