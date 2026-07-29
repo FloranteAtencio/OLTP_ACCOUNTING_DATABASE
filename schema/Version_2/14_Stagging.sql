2939deaef6c94c2384167eb203b4bf3293f6966d
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-07-29 19:20:52 +0800

    kindly check

--- schema/Version_2/14_Stagging.sql
+++ schema/Version_2/14_Stagging.sql
@@ -498,7 +498,7 @@ BEGIN
     UPDATE staging.import_workflows
     SET new_state = 'POSTED',
         previous_state = new_previous_state
-    WHERE session_id = new_session_id AND new_state = 'APPROVED_L3';
+    WHERE session_id = p_session_id AND new_state = 'APPROVED_L3';
 
 EXCEPTION
     WHEN OTHERS THEN
