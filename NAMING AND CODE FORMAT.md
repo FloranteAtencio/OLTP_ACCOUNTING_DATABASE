08a060c0871e67869e9ba26e3f1c848cfd2b56ce
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-02 17:59:18 +0800

    Documentation

---
+++ NAMING AND CODE FORMAT.md
@@ -0,0 +1,5 @@
+# Naming Conventions
+
+* Schema name always start with capital Letter e.g.(Finance, Staging, Compliane)
+* Use " _ " under score instead of space
+* all table name should have 's e.g.(Finance.clients, Finance.account_receivbales, Finance.account_payables) and NO s at column name.. 
--- schema/Version_1/03_Fianance_Table.sql
+++ schema/Version_1/03_Fianance_Table.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS Finance.coa_templates (
 -- ============================================
 DROP TABLE IF EXISTS Finance.coa_template_teaccounts CASCADE;
 CREATE TABLE IF NOT EXISTS Finance.coa_template_accounts (
-    template_account_Id BIGSERIAL PRIMARY KEY,
+    template_account_id BIGSERIAL PRIMARY KEY,
     template_id INT NOT NULL REFERENCES Finance.coa_templates(template_id) ON DELETE NO ACTION,
     account_code INT NOT NULL,
     account_name VARCHAR(255) NOT NULL,
