bb242fc2f66335534d51ea38bf8320a321a60fa4
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-06 05:07:17 +0800

    fuck you

--- tmp/CONFIGURATIONS/SET_up.sql
+++ tmp/CONFIGURATIONS/SET_up.sql
@@ -1,5 +1,5 @@
 -- 00_SETUP_COMPLETE.sql
-\i /Staging_Startup_configuration.sql
+\i ./Staging_Startup_configuration.sql
 -- \i ../../schema/Version_1/03_Finance_Table.sql
 -- \i ../../schema/Version_1/07_Finance_Constraint.sql
 -- \i ../DATA/your_other_scripts.sql
