90910441316dab60990341196c641b31555b5f77
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-06 05:46:06 +0800

    what the fuck

--- tmp/CONFIGURATIONS/SET_up.sql
+++ tmp/CONFIGURATIONS/SET_up.sql
@@ -1,5 +1,5 @@
 -- 00_SETUP_COMPLETE.sql
-\i Staging_Startup_configuration.sql
-\i ../../schema/Version_1/03_Finance_Table.sql
+--\i Staging_Startup_configuration.sql
+\i ../../schema/version_1/03_Finance_Table.sql
 --\i schema/Version_1/07_Finance_Constraint.sql
 --\i migration/Version_1/Multitenant_saas/rls_customized.sql
