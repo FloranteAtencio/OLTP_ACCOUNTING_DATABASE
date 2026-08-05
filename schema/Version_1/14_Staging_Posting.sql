abfc8e6470dfae55639735d6ad9620e2b80a9bed
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-05 22:41:30 +0800

    wow

--- schema/Version_1/14_Staging_Posting.sql
+++ schema/Version_1/14_Staging_Posting.sql
@@ -37,38 +37,34 @@ BEGIN
             gen_random_uuid()::TEXT
         );
 
-        IF current_setting('app.import_source_file', TRUE) IS NOT NULL THEN    
-            INSERT INTO Audit.record_lineage (
-                table_name, 
-                record_id, 
-                client_id, 
-                source_type, 
-                source_file, 
-                import_session_id, 
-                created_by,
-                prev_hash, 
-                row_hash
-            ) VALUES (
-                'stg_ar_import', 
-                r.id, 
-                r.client_code::INT, 
-                'SPREADSHEET_IMPORT',
-                current_setting('app.import_source_file', TRUE),
-                p_session_id::INT,
-                current_user,
-                new_previous_hash,
-                md5(
-                    COALESCE(new_previous_hash,'')
-                    || p_session_id
-                    || 'stg_ar_import'
-                    || 'SPREADSHEET_IMPORT'
-                    || r.id
-                    || current_user
-                )
-            );
-        ELSE
-            RAISE EXCEPTION 'Data Source File is missing!';
-    END IF;
+        INSERT INTO Audit.record_lineage (
+            table_name, 
+            record_id, 
+            client_id, 
+            source_type, 
+            source_file, 
+            import_session_id, 
+            created_by,
+            prev_hash, 
+            row_hash
+        ) VALUES (
+            'stg_ar_import', 
+            r.id, 
+            r.client_code::INT, 
+            'SPREADSHEET_IMPORT',
+            current_setting('app.import_source_file', TRUE),
+            p_session_id::INT,
+            current_user,
+            new_previous_hash,
+            md5(
+                COALESCE(new_previous_hash,'')
+                || p_session_id
+                || 'stg_ar_import'
+                || 'SPREADSHEET_IMPORT'
+                || r.id
+                || current_user
+            )
+        );
 
     END LOOP;
         
