3546fde236c13469c1a79f012fd53aeb52632695
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-06 05:38:05 +0800

    good bye

--- tmp/CONFIGURATIONS/SET_up.sql
+++ tmp/CONFIGURATIONS/SET_up.sql
@@ -1,5 +1,5 @@
 -- 00_SETUP_COMPLETE.sql
 --\i schema/Version_1/03_Finance_Table.sql
 --\i schema/Version_1/07_Finance_Constraint.sql
-\i ./tmp/CONFIGURATION/Staging_Startup_configuration.sql
+\i Staging_Startup_configuration.sql
 --\i migration/Version_1/Multitenant_saas/rls_customized.sql
