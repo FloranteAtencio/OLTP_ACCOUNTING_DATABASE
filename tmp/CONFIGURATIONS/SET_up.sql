e29c0517e4329ccff7e451c2efedcee7c5f34290
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-06 05:26:02 +0800

    ayos lan

--- tmp/CONFIGURATIONS/SET_up.sql
+++ tmp/CONFIGURATIONS/SET_up.sql
@@ -1,6 +1,5 @@
 -- 00_SETUP_COMPLETE.sql
-\i ./tmp/CONFURATION/Staging_Startup_configuration.sql
--- \i /schema/Version_1/03_Finance_Table.sql
--- \i /schema/Version_1/07_Finance_Constraint.sql
--- \i /DATA/your_other_scripts.sql
--- \i migration/Version_1/Multitenant_saas/rls_customized.sql
+--\i schema/Version_1/03_Finance_Table.sql
+--\i schema/Version_1/07_Finance_Constraint.sql
+\i tmp/CONFIGURATION/SET_up.sql
+--\i migration/Version_1/Multitenant_saas/rls_customized.sql
