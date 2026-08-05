3dde2b8a4ae800654907c4408189cc05f2d7dc3f
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-06 05:17:07 +0800

    again

--- tmp/CONFIGURATIONS/SET_up.sql
+++ tmp/CONFIGURATIONS/SET_up.sql
@@ -1,6 +1,6 @@
 -- 00_SETUP_COMPLETE.sql
-\i  ../tmp/CONFURATION/Staging_Startup_configuration.sql
--- \i ../../schema/Version_1/03_Finance_Table.sql
--- \i ../../schema/Version_1/07_Finance_Constraint.sql
--- \i ../DATA/your_other_scripts.sql
+\i /tmp/CONFURATION/Staging_Startup_configuration.sql
+-- \i /schema/Version_1/03_Finance_Table.sql
+-- \i /schema/Version_1/07_Finance_Constraint.sql
+-- \i /DATA/your_other_scripts.sql
 -- \i migration/Version_1/Multitenant_saas/rls_customized.sql
