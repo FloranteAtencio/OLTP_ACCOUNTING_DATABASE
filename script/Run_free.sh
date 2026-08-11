041fe49778d20da598e7deee890641bc0a3fd296
Florante Atencio <144699572+FloranteAtencio@users.noreply.github.com>
2026-08-12 00:29:49 +0800

    Thank

---
+++ script/Run_free.sh
@@ -0,0 +1,9 @@
+docker-compose -f ./docker/docker-compose.prod.yml -p production up
+
+docker exec -it erp_postgres chown postgres:postgres /var/lib/postgresql/wal_archive
+
+docker exec -it erp_postgres chmod 700 /var/lib/postgresql/wal_archive
+
+docker restart erp_postgres
+
+docker exec -i erp_postgres psql -U erp_admin -d erp_db < ./tmp/DATA/data_sample.sql
